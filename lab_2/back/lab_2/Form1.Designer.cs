3bf23cc8205b8c0b7140c1d16fab550b724da0a5
kam1xuseless <80982403+kam1xgod@users.noreply.github.com>
2021-05-04 14:29:58 +0300

    Add files via upload
    actually not working cuz i'm an idiot.  but let it be here.

---
+++ lab_2/back/lab_2/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ram_percents = new System.Windows.Forms.Label();
+            this.virtual_all = new System.Windows.Forms.Label();
+            this.virtual_empty = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 20);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Найти.";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(252, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 4;
+            // 
+            // ram_percents
+            // 
+            this.ram_percents.AutoSize = true;
+            this.ram_percents.Location = new System.Drawing.Point(12, 344);
+            this.ram_percents.Name = "ram_percents";
+            this.ram_percents.Size = new System.Drawing.Size(35, 13);
+            this.ram_percents.TabIndex = 5;
+            this.ram_percents.Text = "label1";
+            // 
+            // virtual_all
+            // 
+            this.virtual_all.AutoSize = true;
+            this.virtual_all.Location = new System.Drawing.Point(12, 322);
+            this.virtual_all.Name = "virtual_all";
+            this.virtual_all.Size = new System.Drawing.Size(35, 13);
+            this.virtual_all.TabIndex = 6;
+            this.virtual_all.Text = "label2";
+            // 
+            // virtual_empty
+            // 
+            this.virtual_empty.AutoSize = true;
+            this.virtual_empty.Location = new System.Drawing.Point(12, 299);
+            this.virtual_empty.Name = "virtual_empty";
+            this.virtual_empty.Size = new System.Drawing.Size(35, 13);
+            this.virtual_empty.TabIndex = 7;
+            this.virtual_empty.Text = "label3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(146, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Записать.";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.virtual_empty);
+            this.Controls.Add(this.virtual_all);
+            this.Controls.Add(this.ram_percents);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label ram_percents;
+        private System.Windows.Forms.Label virtual_all;
+        private System.Windows.Forms.Label virtual_empty;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab_2/back/lab_2/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            load();
+        }
+
+        //апи функция для просмотра памяти
+        [DllImport("kernel32.dll")]
+        //для выделения памяти
+        public static extern IntPtr GlobalAlloc(int con, int size);
+        [DllImport("kernel32.dll")]
+        //для освобождения
+        public static extern int GlobalFree(IntPtr start);
+        [DllImport("kernel32.dll")]
+        public static extern void GlobalMemoryStatus(ref MEMORYSTATUS lpBuffer);
+        //
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int
+            dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+        //
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int
+        hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int dwSize,
+        ref int lpNumberOfBytesRead);
+
+        //
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(int hProcess, int
+            lpBaseAddress,
+            byte[] lpBuffer, int dwSize, ref int
+            lpNumberOfBytesWritten);
+        //
+        public struct MEMORYSTATUS
+        {
+            public UInt32 dwLength;               //Размер структуры, в байтах
+            public UInt32 dwMemoryLoad;           //процент занятой памяти
+            public UInt32 dwTotalPhys;            //общее кол-во физической(оперативной) памяти
+            public UInt32 dwAvailPhys;            //свободное кол-во физической(оперативной) памяти
+            public UInt32 dwTotalPageFile;        //предел памяти для системы или текущего процесса
+            public UInt32 dwAvailPageFile;        //Максимальный объем памяти,который текущий процесс может передать в байтах.
+            public UInt32 dwTotalVirtual;         //общее количество виртуальной памяти(файл подкачки)
+            public UInt32 dwAvailVirtual;         //свободное количество виртуальной памяти(файл подкачки)
+            public UInt32 dwAvailExtendedVirtual; //Зарезервировано. Постоянно 0.
+        }
+        //функция для считывания памяти
+
+
+        public void load()
+
+        {
+            //объявляем переменную типа мемористатус
+            MEMORYSTATUS memStatus = new MEMORYSTATUS();
+            //вызываем функцию с аргументом мемори статус
+            GlobalMemoryStatus(ref memStatus);
+
+
+            ram_percents.Text = "Используемая память: " + memStatus.dwMemoryLoad.ToString() + " %";
+            virtual_all.Text = "Общая виртуальная память: " + (memStatus.dwTotalVirtual).ToString();
+            virtual_empty.Text = "Свободная виртуальная память: " + memStatus.dwAvailVirtual.ToString();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string[] temp = comboBox1.SelectedItem.ToString().Split(' ');
+            int ProcessID = Convert.ToInt32(temp[0]);
+            Process process = Process.GetProcessById(ProcessID);
+            IntPtr processHandle = OpenProcess(0x0010, false, process.Id);
+            int bytesWritten = 0;
+            byte[] buffer = Encoding.Unicode.GetBytes(textBox1.Text + "\0");
+            WriteProcessMemory((int)processHandle, Int32.Parse(textBox1.Text, System.Globalization.NumberStyles.HexNumber),
+                buffer, buffer.Length, ref bytesWritten);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] temp = comboBox1.SelectedItem.ToString().Split(' ');
+            int ProcessID = Convert.ToInt32(temp[0]);
+            Process process = Process.GetProcessById(ProcessID);
+            IntPtr processHandle = OpenProcess(0x0010, false, process.Id);
+            int length = Encoding.Unicode.GetBytes(textBox2.Text + "\0").Length;
+            int bytesRead = 0;
+            byte[] buffer = new byte[length];
+            ReadProcessMemory((int)processHandle, Int32.Parse(textBox1.Text, System.Globalization.NumberStyles.HexNumber), buffer, buffer.Length, ref bytesRead);
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            load();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (Process instance in processes)
+            {
+                comboBox1.Items.Add(new ProcessInfo(instance));
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        class ProcessInfo
+        {
+            Process _process;
+            public ProcessInfo(Process process)
+            {
+                _process = process;
+            }
+            public override string ToString() { return string.Format("{0} {1}", _process.Id, _process.ProcessName); }
+            public void Kill()
+            {
+                _process.Refresh();
+                if (!_process.HasExited)
+                {
+                    _process.Kill();
+                }
+            }
+        }
+    }
+}
