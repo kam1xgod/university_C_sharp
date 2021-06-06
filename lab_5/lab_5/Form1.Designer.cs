9507fbf8cd35911eca958d4a2c30865ae50e87a9
kam1xuseless <80982403+kam1xgod@users.noreply.github.com>
2021-06-06 10:12:41 +0300

    added lab_5.

---
+++ lab_5/lab_5/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace lab_5
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.StartWithoutLockButton = new System.Windows.Forms.Button();
+            this.StartWithLockButton = new System.Windows.Forms.Button();
+            this.ActionsTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // StartWithoutLockButton
+            // 
+            this.StartWithoutLockButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StartWithoutLockButton.Font = new System.Drawing.Font("Ubuntu Mono", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartWithoutLockButton.Location = new System.Drawing.Point(416, 12);
+            this.StartWithoutLockButton.Name = "StartWithoutLockButton";
+            this.StartWithoutLockButton.Size = new System.Drawing.Size(110, 33);
+            this.StartWithoutLockButton.TabIndex = 0;
+            this.StartWithoutLockButton.Text = "w/out";
+            this.StartWithoutLockButton.UseVisualStyleBackColor = true;
+            this.StartWithoutLockButton.Click += new System.EventHandler(this.StartWithoutLockButton_Click);
+            // 
+            // StartWithLockButton
+            // 
+            this.StartWithLockButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StartWithLockButton.Font = new System.Drawing.Font("Ubuntu Mono", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartWithLockButton.Location = new System.Drawing.Point(300, 12);
+            this.StartWithLockButton.Name = "StartWithLockButton";
+            this.StartWithLockButton.Size = new System.Drawing.Size(110, 33);
+            this.StartWithLockButton.TabIndex = 1;
+            this.StartWithLockButton.Text = "w/";
+            this.StartWithLockButton.UseVisualStyleBackColor = true;
+            this.StartWithLockButton.Click += new System.EventHandler(this.StartWithLockButton_Click);
+            // 
+            // ActionsTextBox
+            // 
+            this.ActionsTextBox.BackColor = System.Drawing.Color.White;
+            this.ActionsTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ActionsTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ActionsTextBox.Name = "ActionsTextBox";
+            this.ActionsTextBox.Size = new System.Drawing.Size(282, 596);
+            this.ActionsTextBox.TabIndex = 4;
+            this.ActionsTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::lab_5.Properties.Resources.rem_png_by_mikaya_chan_dbjc2l8_pre;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(954, 846);
+            this.Controls.Add(this.ActionsTextBox);
+            this.Controls.Add(this.StartWithLockButton);
+            this.Controls.Add(this.StartWithoutLockButton);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "lab_5";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartWithoutLockButton;
+        private System.Windows.Forms.Button StartWithLockButton;
+        private System.Windows.Forms.RichTextBox ActionsTextBox;
+    }
+}
+
---
+++ lab_5/lab_5/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace lab_5
+{
+    public partial class MainForm : Form
+    {
+        private bool isStarted = false;
+
+        Thread Dasha;
+        Thread Masha;
+
+        Object scissors = new Object();
+        Object paper = new Object();
+        private bool lockTaken = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Thread.CurrentThread.Name = "Main";
+        }
+
+        private void StartWithoutLockButton_Click(object sender, EventArgs e)
+        {
+            if (!isStarted)
+            {
+                Object scissors = new Object();
+                Object paper = new Object();
+                isStarted = true;
+                Dasha = new Thread(() => startMakingSKittyWithoutLock(scissors, paper));
+                Masha = new Thread(() => startMakingDoggyWithoutLock(scissors, paper));
+                Dasha.Name = "Dasha";
+                Masha.Name = "Masha";
+                Dasha.Start();
+                Masha.Start();
+                return;
+            }
+            isStarted = false;
+        }
+
+        private void StartWithLockButton_Click(object sender, EventArgs e)
+        {
+            if (!isStarted)
+            {
+                isStarted = true;
+                Dasha = new Thread(() => startMakingKitty(scissors, paper));
+                Masha = new Thread(() => startMakingDoggy(scissors, paper));
+                Dasha.Name = "Dasha";
+                Masha.Name = "Masha";
+                Dasha.Start();
+                Masha.Start();
+                return;
+            }
+            isStarted = false;
+        }
+
+        private void startMakingKitty(Object scissors, Object paper)
+        {
+            string temp;
+            while (isStarted)
+            {
+                lock(scissors)
+                {
+                    temp = $"{Thread.CurrentThread.Name} took scissors.\n";
+                    ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                    Thread.Sleep(1000);
+                    temp = $"{ Thread.CurrentThread.Name } waiting for another instrument.\n";
+                    ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                    Thread.Sleep(1000);
+                    lockTaken = false;
+                    detectDeadlock(paper);
+                    lock (paper)
+                    {
+                        temp = $"{Thread.CurrentThread.Name} took paper.\n";
+                        ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                        Thread.Sleep(1000);
+                        temp = $"{ Thread.CurrentThread.Name } waiting for another instrument.\n";
+                        ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        private void startMakingDoggy(Object scissors, Object paper)
+        {
+            string temp;
+            while (isStarted)
+            {
+                lock (paper)
+                {
+                    temp = $"{Thread.CurrentThread.Name} took paper.\n";
+                    ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                    Thread.Sleep(1000);
+                    temp = $"{ Thread.CurrentThread.Name } waiting for another instrument.\n";
+                    ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                    Thread.Sleep(1000);
+                    lockTaken = false;
+                    detectDeadlock(scissors);
+                    lock (scissors)
+                    {
+                        temp = $"{Thread.CurrentThread.Name} took scissors.\n";
+                        ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                        Thread.Sleep(1000);
+                        temp = $"{ Thread.CurrentThread.Name } waiting for another instrument.\n";
+                        ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+        
+        private void startMakingSKittyWithoutLock(Object scissors, Object paper)
+        {
+            string temp;
+            while (isStarted)
+            {
+                lock(paper)
+                {
+                    temp = $"{Thread.CurrentThread.Name} took paper.\n";
+                    ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                }
+                Thread.Sleep(1000);
+                temp = $"{ Thread.CurrentThread.Name } waiting for another instrument.\n";
+                ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                Thread.Sleep(1000);
+                lock (scissors) {
+                    temp = $"{Thread.CurrentThread.Name} took scissors.\n";
+                    ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                }
+                Thread.Sleep(1000);
+                temp = $"{ Thread.CurrentThread.Name } waiting for another instrument.\n";
+                ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void startMakingDoggyWithoutLock(Object scissors, Object paper)
+        {
+            string temp;
+            while (isStarted)
+            {
+                lock(scissors)
+                {
+                    temp = $"{Thread.CurrentThread.Name} took scissors.\n";
+                    ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                }
+                Thread.Sleep(1000);
+                temp = $"{ Thread.CurrentThread.Name } waiting for another instrument.\n";
+                ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                Thread.Sleep(1000);
+                lock (paper) {
+                    temp = $"{Thread.CurrentThread.Name} took paper.\n";
+                    ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                }
+                Thread.Sleep(1000);
+                temp = $"{ Thread.CurrentThread.Name } waiting for another instrument.\n";
+                ActionsTextBox.BeginInvoke(new Action(() => ActionsTextBox.AppendText(temp)));
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void detectDeadlock(Object resY)
+        {
+            try
+            {
+                Monitor.TryEnter(resY, 1000, ref lockTaken);
+                if (lockTaken)
+                {
+                    ActionsTextBox.Invoke(new Action(() => ActionsTextBox.AppendText($"Nah. Not a deadlock.\n")));
+                    return;
+                }
+                ActionsTextBox.Invoke(new Action(() => ActionsTextBox.AppendText($"Congratulations! You've reached deadlock, buddy.\n")));
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Monitor.Exit(resY);
+                }
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Dasha != null && Masha != null)
+            {
+                if (Dasha.IsAlive && Masha.IsAlive)
+                {
+                    Dasha.Abort();
+                    Masha.Abort();
+                }
+            }
+            //very funny.
+        }
+    }
+}
